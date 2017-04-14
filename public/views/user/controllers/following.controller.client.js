b34398683d4ad89590d4a354d2cfa97f29837cea
asimkhan17790 <asimkhan17790@gmail.com>
2017-04-14 12:37:08 -0400

    linking of top most level pages of desi user done

--- public/views/user/controllers/following.controller.client.js
+++ public/views/user/controllers/following.controller.client.js
@@ -6,7 +6,7 @@
         .module("WebDevMusicApp")
         .controller("FollowingController", FollowingController);
 
-    function FollowingController ($location ,$routeParams ,UserService ,Upload) {
+    function FollowingController ($location ,$routeParams ,UserService ,$timeout) {
         var vm = this;
         vm.userId = $routeParams['uid'];
         vm.following = null;
@@ -14,7 +14,11 @@
         vm.users = null;
         vm.searchUsers = searchUsers ;
         vm.clearUserFromModal  = clearUserFromModal;
+        vm.followersLength=null;
+        vm.searchUsers = searchUsers ;
+        vm.redirectToSearchedUser  = redirectToSearchedUser;
         function init() {
+            getUserDetails();
             var promise = UserService.findFollowingById(vm.userId);
             promise.success (function (result) {
                 if (result && result.status==='OK' && result.data && result.data.length >0) {
@@ -31,6 +35,22 @@
 
         }
         init();
+
+        function getUserDetails() {
+            var promise = UserService.findUserById(vm.userId);
+            promise.success (function (result) {
+                if (result && result.status==='OK' && result.data) {
+                    vm.followers = result.data.followers.length ;
+                    vm.followingLength = result.data.following.length ;
+                    vm.error = null;
+                } else {
+                    vm.error = "Some Error Occurred!! Please try again!";
+                }
+
+            }).error(function () {
+                vm.error = "Some Error Occurred!! Please try again!";
+            });
+        }
         function searchUsers () {
             var promise = UserService.searchUsers(vm.inputQuery);
             promise.success (function (result) {
@@ -43,6 +63,34 @@
                 }
             }).error(function () {
                 vm.users = null;
+                vm.error = "Some Error Occurred!! Please try again!";
+            });
+        }
+        function closeModal() {
+
+            $('.modal').modal('hide');
+        }
+        function redirectToSearchedUser(userId2) {
+
+            var promise = UserService.findUserById(userId2);
+            promise.success (function (result) {
+                if (result && result.status==='OK' && result.data) {
+                    var searchedUser = result.data;
+                    closeModal();
+                    $timeout(function () {
+                        if(searchedUser.userType == 'U')
+                            $location.url("/user/userSearch/"+vm.userId+"/"+userId2);
+                        else if(searchedUser.userType == 'M')
+                        {
+                            $location.url("/user/singerSearch/"+vm.userId+"/"+userId2);
+                        }
+                    }, 250);
+                    vm.error = null;
+                } else {
+                    vm.error = "Some Error Occurred!! Please try again!";
+                }
+
+            }).error(function () {
                 vm.error = "Some Error Occurred!! Please try again!";
             });
         }
