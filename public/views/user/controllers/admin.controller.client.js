0059262b879d82006f61a7d391257c8491070bb2
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2017-04-14 13:00:13 -0400

    changes

---
+++ public/views/user/controllers/admin.controller.client.js
@@ -0,0 +1,102 @@
+/**
+ * Created by sumitbhanwala on 4/14/17.
+ */
+(function() {
+
+    angular
+        .module("WebDevMusicApp")
+        .run(function($rootScope, $uibModalStack) {
+            $uibModalStack.dismissAll();
+        })
+        .controller("adminPageController",adminPageController);
+
+    function adminPageController ($scope,EventService ,$sce,UserService ,$routeParams ,MusicService,$timeout,playListService,$location) {
+        var vm = this;
+        vm.userId = $routeParams.uid ;
+        vm.editProfile = editProfile ;
+        vm.error = null;
+        vm.getTrsustedURL = getTrsustedURL;
+        vm.searchUsers = searchUsers ;
+        vm.clearUserFromModal  = clearUserFromModal;
+        vm.redirectToSearchedUser  = redirectToSearchedUser
+        function init() {
+            getUserDetails ();
+        }
+        init();
+
+        function clearUserFromModal() {
+            vm.users = null;
+            vm.error = null;
+            vm.inputQuery = null;
+        }
+
+        function redirectToSearchedUser(userId2) {
+            var promise = UserService.findUserById(userId2);
+            promise.success (function (result) {
+                if (result && result.status==='OK' && result.data) {
+                    var searchedUser = result.data;
+                    closeModal();
+                    $timeout(function () {
+                        if(searchedUser.userType == 'U')
+                            $location.url("/user/userSearch/"+vm.userId+"/"+userId2);
+                        else if(searchedUser.userType == 'M')
+                        {
+                            $location.url("/user/singerSearch/"+vm.userId+"/"+userId2);
+                        }
+                    }, 250);
+                    vm.error = null;
+                } else {
+                    vm.error = "Some Error Occurred!! Please try again!";
+                }
+            }).error(function () {
+                vm.error = "Some Error Occurred!! Please try again!";
+            });
+        }
+
+
+        function searchUsers () {
+            var promise = UserService.searchUsers(vm.inputQuery);
+            promise.success (function (result) {
+                if (result && result.status==='OK' && result.data && result.data.length >0) {
+                    vm.users = result.data;
+                    vm.error = null;
+                } else {
+                    vm.users = null;
+                    vm.error = "No user found !!";
+                }
+            }).error(function () {
+                vm.users = null;
+                vm.error = "Some Error Occurred!! Please try again!";
+            });
+        }
+
+        function getUserDetails() {
+            var promise = UserService.findUserById(vm.userId);
+            promise.success (function (result) {
+                if (result && result.status==='OK' && result.data) {
+                    vm.user = result.data;
+                    vm.error = null;
+                } else {
+                    vm.error = "Some Error Occurred!! Please try again!";
+                }
+
+            }).error(function () {
+                vm.error = "Some Error Occurred!! Please try again!";
+            });
+        }
+
+        function editProfile() {
+            $location.url("/user/editProfile/"+vm.userId);
+        }
+
+        function closeModal() {
+            $('.modal').modal('hide');
+        }
+
+
+        function getTrsustedURL (url) {
+            return  $sce.trustAsResourceUrl(url);
+        }
+    }
+
+})();
