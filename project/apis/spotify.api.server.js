c1f5461f69cd8f9dfbcf01e79fbb6ebf504cda38
Asim Khan <asimkhan17790@users.noreply.github.com>
2018-01-22 06:12:14 -0500

    updated env variables for spotify redirect URI

--- project/apis/spotify.api.server.js
+++ project/apis/spotify.api.server.js
@@ -13,7 +13,7 @@ module.exports = function () {
     var spotifyApi = new SpotifyWebApi({
         clientId: process.env.SPOTIFY_CLIENT_ID,
         clientSecret:  process.env.SPOTIFY_CLIENT_SECRET,
-        redirectUri: 'http://localhost:3000/callback',
+        redirectUri: process.env.SPOTIFY_REDIRECT_URI,
     });
 
 
